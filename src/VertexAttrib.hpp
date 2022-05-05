5cbc63da8c752e62ebf1d3231b5eb4034d3def42
FanWang10 <wang.13244@osu.edu>
2022-05-04 20:32:45 -0400

    The Square Now Change Its Color With Time

---
+++ src/VertexAttrib.hpp
@@ -0,0 +1,9 @@
+#ifndef VERTEXATTRIB_HPP
+#define VERTEXATTRIB_HPP
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// return VAO
+unsigned int configureVertexAttribute(float * vertices, int sizeVertices, unsigned int * indices, int sizeIndices);
+
+#endif
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,7 +1,8 @@
 #include "Window.hpp"
 #include "Shader.hpp"
+#include "VertexAttrib.hpp"
 #include <iostream>
-
+#include <math.h>
 
 const char *vertexShaderSource = 
 "#version 330 core\n"
@@ -14,9 +15,10 @@ const char *vertexShaderSource =
 const char *fragmentShaderSource = 
 "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
 "}\0"
 ;
 
@@ -76,7 +78,6 @@ void Window::Initialize()
            glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
        }
    }
-
 }
 
 void Window::Terminate()
@@ -103,45 +104,31 @@ void Window::OnExecute()
         1, 2, 3
     };
 
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-
-
+    unsigned int VAO = configureVertexAttribute(vertices, sizeof(vertices), indices, sizeof(indices));
 
     while(!glfwWindowShouldClose(window))
 	{
         glUseProgram(shaderProgram);
 
-		// rendering command
+		// draw background
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+        // update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-        
-
-        //-----------------------------------
+        // draw out first triangle
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);        
+        // -----------------------------------
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		processInput(window);
 	}
-
     Terminate();
 
 }
