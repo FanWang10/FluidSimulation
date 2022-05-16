d396ea52e864f80c8e31590e1f34a0f4bbbd3b55
FanWang10 <wang.13244@osu.edu>
2022-05-16 17:06:58 -0400

    Remove

--- src/Shader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SHADER_HPP
-#define SHADER_HPP
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-unsigned int LoadShaders(const char* vertexShaderSrc, const char* fragShaderSrc);
-
-#endif
--- src/VertexAttrib.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VERTEXATTRIB_HPP
-#define VERTEXATTRIB_HPP
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-// return VAO
-unsigned int configureVertexAttribute(float * vertices, int sizeVertices);
-
-#endif
